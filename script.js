7d66c913f3a3794c6e8b2bcb43633f9d57eb5732
Felipe Smith <fsmith95@hotmail.com>
2016-12-16 17:45:12 -0300

    Cambiamo  lo colore

--- script.js
+++ script.js
@@ -13,11 +13,6 @@ $(document).ready(function(){
     if(this.hash){
       var hash = this.hash.substr(1);
       console.log(hash);
-      // var toElement = $("."+hash);
-      // console.log(toElement);
-      // var toPosition = toElement.offset().top;
-      // console.log(toPosition);
-      // var pos = $('unId').position().top;
       $("body,html").animate({
         scrollTop : $('#'+hash).offset().top -100
       },2000);
